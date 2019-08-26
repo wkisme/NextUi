12f7a6cf15ba783b16cee368cfd908670db7c7eb
wkisme <31771166+wkisme@users.noreply.github.com>
2019-08-26 21:08:28 +0800

    Add files via upload

---
+++ dnac-nbapi-mission/next-data-mission.js
@@ -0,0 +1,76 @@
+/*DO NOT EDIT - NeXt Topology file generated from DNA Center Device and Module Inventory*/
+
+var topologyData = 
+{
+    "links": [
+        {
+            "source": 0,
+            "target": 1
+        },
+        {
+            "source": 0,
+            "target": 2
+        },
+        {
+            "source": 3,
+            "target": 4
+        },
+        {
+            "source": 3,
+            "target": 5
+        }
+    ],
+    "nodes": [
+        {
+            "icon": "switch",
+            "id ": 0,
+            "name": "cat_9k_1",
+            "platform": "C9300-24UX",
+            "serial": "FCW2136L0AK",
+            "x": 0,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 1,
+            "name": "Switch 1 - C9300-24UX - Fixed Module 0",
+            "serial": "FCW2136L0AK",
+            "x": 20,
+            "y": 40
+        },
+        {
+            "icon": "server",
+            "id ": 2,
+            "name": "8x10G Uplink Module",
+            "serial": "FCW2136L0AK",
+            "x": 40,
+            "y": 60
+        },
+        {
+            "icon": "switch",
+            "id ": 3,
+            "name": "cat_9k_2",
+            "platform": "C9300-24UX",
+            "serial": "FCW2140L039",
+            "x": 60,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 4,
+            "name": "Switch 1 - C9300-24UX - Fixed Module 0",
+            "serial": "FCW2140L039",
+            "x": 80,
+            "y": 40
+        },
+        {
+            "icon": "server",
+            "id ": 5,
+            "name": "8x10G Uplink Module",
+            "serial": "FCW2140L039",
+            "x": 100,
+            "y": 60
+        }
+    ]
+}
+/*DO NOT EDIT - EOF*/
